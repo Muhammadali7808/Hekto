7f885ad7c3206f8983f2e5f1304e5c4eb6319bb9
Muhammadali0808 <umarovasayyora70@gmail.com>
2024-11-04 17:13:15 +0500

    Hekto

---
+++ src/data/date.js
@@ -0,0 +1,109 @@
+export const hero = [
+    {
+      text: "Best Furniture For Your Castle....",
+      h2: "New Furniture Collection Trends in 2020",
+      p: "Lorem ipsum dolor sit amet, consectetur adipiscing  elit. Magna in est adipiscingin <br> phasellus non in justo.",
+      img: "./img/img_6.png",
+    },
+  ];
+  
+  export const box = [
+    {
+      img: "./img/img_7.png",
+      h3: "Cantilever chair",
+      img_1: "./img/img_8.svg",
+      text: "Code - Y523201",
+      id:4,
+      strong: 47,
+    },
+    {
+      img: "./img/img_7.png",
+      h3: "Cantilever chair",
+      img_1: "./img/img_8.svg",
+      text: "Code - Y523201",
+      id:1,
+      strong: 42,
+    },
+    {
+      img: "./img/img_7.png",
+      h3: "Cantilever chair",
+      img_1: "./img/img_8.svg",
+      text: "Code - Y523201",
+      id:2,
+      strong: 62,
+    },
+    {
+      img: "./img/img_7.png",
+      h3: "Cantilever chair",
+      img_1: "./img/img_8.svg",
+      text: "Code - Y523201",
+      id:3,
+      strong: 55,
+    },
+  ];
+  
+  export const liener = [
+    {
+      img: "./img/img_10.png",
+      text: "Comfort Handy Craft",
+      discoreption: "$42.00",
+      strong: "$65.00",
+    },
+    {
+      img: "./img/img_10.png",
+      text: "Comfort Handy Craft",
+      discoreption: "$42.00",
+      strong: "$65.00",
+    },
+    {
+      img: "./img/img_10.png",
+      text: "Comfort Handy Craft",
+      discoreption: "$42.00",
+      strong: "$65.00",
+    },
+    {
+      img: "./img/img_10.png",
+      text: "Comfort Handy Craft",
+      discoreption: "$42.00",
+      strong: "$65.00",
+    },
+    {
+      img: "./img/img_10.png",
+      text: "Comfort Handy Craft",
+      discoreption: "$42.00",
+      strong: "$65.00",
+    },
+    {
+      img: "./img/img_10.png",
+      text: "Comfort Handy Craft",
+      discoreption: "$42.00",
+      strong: "$65.00",
+    },
+  ];
+  export const product = [
+    {
+      img: "./img/img_14.jpg",
+      h3: "Cantilever chair",
+      p: "$26.00 ",
+      strong: "$42.00",
+    },
+    {
+      img: "./img/img_14.jpg",
+      h3: "Cantilever chair",
+      p: "$26.00 ",
+      strong: "$42.00",
+    },
+    {
+      img: "./img/img_14.jpg",
+      h3: "Cantilever chair",
+      p: "$26.00 ",
+      strong: "$42.00",
+    },
+    {
+      img: "./img/img_14.jpg",
+      h3: "Cantilever chair",
+      p: "$26.00 ",
+      strong: "$42.00",
+    },
+  ];
+  
